f4f1f84b634408880fb2db31584d2866f49f6b7e
lucasalbuquerques <lucasalbuquerque01@gmail.com>
2022-01-06 20:37:34 -0300

    Day 4

---
+++ Day-04/example01.py
@@ -0,0 +1,4 @@
+import random
+
+randon_integer = random.randint(1 ,10)
+print(randon_integer)
---
+++ Day-04/exercise1.py
@@ -0,0 +1,15 @@
+import random
+#Remember to use the random module
+#Hint: Remember to import the random module here at the top of the file. 🎲
+	 
+# 🚨 Don't change the code below 👇
+test_seed = int(input("Create a seed number: "))
+random.seed(test_seed)
+ # 🚨 Don't change the code above 👆 It's only for testing your code.
+	 
+#Write the rest of your code below this line 👇
+number = random.randint(0,1)
+if number == 0:
+    print("Tails")
+else:
+    print("Heads")
