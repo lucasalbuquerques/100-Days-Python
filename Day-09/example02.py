7dbf58b83a8bad2d61dfced15ff554865cb14689
lucasalbuquerques <lucasalbuquerque01@gmail.com>
2022-01-20 14:55:58 -0300

    Merge remote-tracking branch 'origin/main'

---
+++ Day-09/example02.py
@@ -0,0 +1,4 @@
+travel_log = {
+    "France": {"cities_visited": ["Paris", "Lilie", "Dijon"], "total_visited": 12},
+    "Germany": {"cities_visited": ["Berlin", "Munich", "Frankfurt"], "total_visited:": 5},
+}
