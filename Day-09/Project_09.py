a6536bd6c830d78e36464db09496d893d005460d
lucasalbuquerques <lucasalbuquerque01@gmail.com>
2022-01-20 17:01:29 -0300

    Merge remote-tracking branch 'origin/main'

---
+++ Day-09/Project_09.py
@@ -0,0 +1,43 @@
+import art
+import os
+
+def clear():
+    os.system('cls' if os.name=='nt' else 'clear')
+
+
+print(art.logo)
+print("Welcome to the secret auction program.")
+
+auction = {}
+next = True
+
+while next == True:
+    name = input("Whats your name? ")
+    bid = int(input("What's your bid? "))
+    auction[f'{name}'] = bid
+
+    continues = input("Are there any other bidders? Type 'yes' or 'no'".lower())
+    if continues == "no":
+        next = False
+        clear()
+    elif continues == "yes":
+        next = True
+        clear()
+    else:
+        print("I dont understand your option, so I will finish")
+        next = False
+        clear()
+
+
+winner_bid = 0
+for key in auction:
+    if winner_bid < auction[key]:
+        winner_bid = auction[key]
+        pearson_winner = key
+print(f"The winner is {pearson_winner} with a bid of ${winner_bid}")
+input("Press enter to exit ;)")
+
+
+
+
+
