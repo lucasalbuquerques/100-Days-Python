67ea257632eef677a4e8337d247e51f8ac29fbc3
lucasalbuquerques <lucasalbuquerque01@gmail.com>
2022-01-28 15:33:07 -0300

    Day 12 Project v1 - insert def guess_game()

--- Day-12/Project_12.py
+++ Day-12/Project_12.py
@@ -1,5 +1,7 @@
 import random
 from art import logo
+
+
 def set_difficulty(level):
     if level == "easy":
         return 10
@@ -9,38 +11,39 @@ def set_difficulty(level):
         print("I didn't recognize your option, so it will be the 'easy' option ")
         return 10
 
-awnser = random.randint(1,100)
 
-print(logo)
-print("Welcome to the Number Guessing Game!")
-print("I'm thinking of a number between 1 and 100.")
-level = input("Chose a difficulty. Type 'easy' or 'hard': ").lower()
-attempt = set_difficulty(level)
+def guess_game():
+    awnser = random.randint(1,100)
+    print(logo)
+    print("Welcome to the Number Guessing Game!")
+    print("I'm thinking of a number between 1 and 100.")
+    level = input("Chose a difficulty. Type 'easy' or 'hard': ").lower()
+    attempt = set_difficulty(level)
 
-contador = 0
+    contador = 0
 
-while contador != attempt:
-    print(f"You have {attempt - contador} attempts remaining to guess the number.")
-    guess = int(input("Make a guess: "))
-    if guess == awnser:
-        print(f"You got it! The awnser was {guess}")
-        contador = attempt - 1
-    elif guess < awnser:
-        if contador == attempt - 1:
-            print("Too low")
-            print("You've run out of guesses, you lose.")
-        else:
-            print("Too low.")
-            print("Guess again.")
-    elif guess > awnser:
-        if contador == attempt - 1:
-            print("Too high")
-            print("You've run out of guesses, you lose.")
-        else:
-            print("Too high.")
-            print("Guess again.")
-    contador = contador + 1
+    while contador != attempt:
+        print(f"You have {attempt - contador} attempts remaining to guess the number.")
+        guess = int(input("Make a guess: "))
+        if guess == awnser:
+            print(f"You got it! The awnser was {guess}")
+            contador = attempt - 1
+        elif guess < awnser:
+            if contador == attempt - 1:
+                print("Too low")
+                print("You've run out of guesses, you lose.")
+            else:
+                print("Too low.")
+                print("Guess again.")
+        elif guess > awnser:
+            if contador == attempt - 1:
+                print("Too high")
+                print("You've run out of guesses, you lose.")
+            else:
+                print("Too high.")
+                print("Guess again.")
+        contador = contador + 1
 
-
+guess_game()
 
 
